3fce15aa316b1107cbcf59e486ee7e88995ec6df
Hazratqul21 <xazratabduraufov@gmail.com>
2026-01-30 13:36:13 +0500

    ai

--- src/core/config.py
+++ src/core/config.py
@@ -8,7 +8,11 @@ class Settings(BaseSettings):
     DEBUG_MODE: bool = False
     
     # Credentials
+    # Credentials
     BINOTEL_API_KEY: str = "70206a-84faf4d"
+    BINOTEL_API_SECRET: str = "e4a051-9d3c02-7cdb1a-a5d224-f8406eda"
+    HELPDESKEDDY_URL: str = "https://qwatt.helpdeskeddy.com/api/v2/telephony/calls/DyJmRuiZTsqsXyRsegJR"
+    
     # AI & DB
     OPENAI_API_KEY: str = "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"
     
